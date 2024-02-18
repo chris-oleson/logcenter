7f2ff30ca9ba60dbb9b68ab61881b31965fbf7a2
Chris Oleson <crolesonjr@gmail.com>
2024-02-18 14:46:50 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <TopBar></TopBar>
+</template>
+
+<script setup>
+    import TopBar from './components/TopBar'
+</script>
+
+<style>
+:root {
+    --shadow: 0px 5px 5px rgba(0, 0, 0, 0.5);
+}
+
+body {
+    font-family: roboto;
+    font-weight: 300;
+    background-color: #fff;
+}
+</style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="top-bar">
+        <img class="logo" src="/src/assets/logo.png">
+        <div class="logo">LogCenter</div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .top-bar {
+        display: flex;
+        align-items: center;
+        height: 50px;
+        box-shadow: var(--shadow);
+    }
+
+    .logo {
+        font-size: 24px;
+    }
+
+    img {
+        height: 32px;
+        width: 32px;
+        padding: 9px;
+    }
+</style>
